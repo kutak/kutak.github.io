0b2161224a51f2ff9c0930dae510aae5994e3bc7
Tomas Patoprsty <patoprsty@webmake.sk>
2014-03-02 21:39:06 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//
+//	jQuery Validate example script
+$(window).load(function () {
+    "use strict";
+   	$("#status").fadeOut();
+	$("#preloader").delay(350).fadeOut("slow");
+});
+	
+$(document).ready(function(){
+
+	// Validate
+	// http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+	// http://docs.jquery.com/Plugins/Validation/
+	// http://docs.jquery.com/Plugins/Validation/validate#toptions
+
+		$('#contact-form').validate({
+	    rules: {
+	      name: {
+	        minlength: 2,
+	        required: true
+	      },
+	      email: {
+	        required: true,
+	        email: true
+	      },
+	      subject: {
+	      	minlength: 2,
+	        required: true
+	      },
+	      message: {
+	        minlength: 2,
+	        required: true
+	      }
+	    },
+			highlight: function(element) {
+				$(element).closest('.form-group').removeClass('success').addClass('error');
+			},
+			success: function(element) {
+				element
+				.text('OK!').addClass('valid')
+				.closest('.form-group').removeClass('error').addClass('success');
+			}
+	  });
+
+}); // end document.ready
